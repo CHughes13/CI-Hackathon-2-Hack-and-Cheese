ff91b9e3217c767c8884dc290475569943ebb73b
Lewis Freeman <lewis_3man@hotmail.com>
2024-07-02 08:37:56 +0000

    Assign id's to variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const startButton = document.getElementById("start-button")
+    const retryButton = document.getElementById("retry-button")
+    const welcomeScreen = document.getElementById("welcome-screen")
+    const quizScreen = document.getElementById("quiz-screen")
+    const incorrectScreen = document.getElementById("incorrect-screen")
+    const questionText = document.getElementById("question-text")
+});
